f5c4c347b1b33c9932fa029a116bf755fcfd06d6
g20207 <gururathinavel20207@gmail.com>
2023-03-11 14:23:19 +0530

    pageTemplates

--- src/app/components/campus/campus.component.ts
+++ src/app/components/campus/campus.component.ts
@@ -7,22 +7,18 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./campus.component.css']
 })
 export class CampusComponent implements OnInit {
-  campusForm: FormGroup;
-public getForm = [];
+
   constructor(private formBuilder: FormBuilder,
     private router: Router,
     private route: ActivatedRoute) {
-      this.campusForm = this.formBuilder.group({
-        campus: ["", [Validators.required,Validators.minLength(3)]],
-      });
+
     }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    this.getForm = this.campusForm.value;
-    console.log(this.getForm);
+    
     }
 
 }
